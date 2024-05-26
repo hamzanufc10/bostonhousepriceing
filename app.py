9bb8ea5383f0bacf311ff2a6d30b4fd5f0afebfe
Hamza Shakeel <hamza.9595@gmail.com>
2024-05-26 11:49:21 +0530

    web app ready

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ scaler=pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
@@ -22,6 +22,14 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
 if __name__=="__main__":
